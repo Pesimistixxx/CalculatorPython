a0948bf3018d44e58b42c811015e96b309aa392a
Pesimistixxx <musienkoiy@gmail.com>
2024-10-14 23:28:34 +0300

    added fractionals

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 def inputCalculations(inp):
-    c = 0
+    c = 0.0
+    res = 0
+    maxlen = 0
+    isfrac = False
+    iszero = False
     nums = []
     for elem in inp:
-        if type(strtoint[elem]) == int:
-            c += strtoint[elem]
+        if elem == "и":
+            isfrac = True
+        elif elem in strtoint:
+            if type(strtoint[elem]) == int:
+                if strtoint[elem] == 0:
+                    iszero = True
+                if isfrac == False:
+                    c += strtoint[elem]
+                else:
+                    maxlen = max(len(str(strtoint[elem])), maxlen)
+                    c += strtoint[elem] / (10 ** (maxlen))
+                #print(c)
+            elif elem == "плюс" or elem == "минус" or elem == "умножить" or elem == "делить" or elem == "остаток":
+                nums.append(c)
+                isfrac = False
+                maxlen = 0
+                nums.append(strtoint[elem])
+                c = 0.0
+        elif elem == "деления" or elem == "от" or elem == "на" or elem in fracword2 or elem in fracword:
+            continue
         else:
-            nums.append(c)
-            nums.append(strtoint[elem])
-            c = 0
+            return "Неправильный Ввод"
     nums.append(c)
-    for i in range(0,len(nums)-2,3):
+    if iszero:
+        if len([item for item in nums if item != 0]) <= 1:
+            return "Неправильный Ввод"
+    else:
+        if len([item for item in nums if item != 0]) <= 2:
+            return "Неправильный Ввод"
+    for i in range(0, len(nums)-2, 3):
         firstop = nums[i]
         secondop = nums[i+2]
         if nums[i + 1] == "+":
@@ -18,26 +44,48 @@ def inputCalculations(inp):
             res = firstop - secondop
         elif nums[i + 1] == "*":
             res = firstop * secondop
+        elif nums[i+1] == "/":
+            if secondop != 0:
+                res = firstop/secondop
+            else:
+                return "Ошибка деления на 0"
+        elif nums[i+1] == "%":
+            res = firstop % secondop
     return res
+
+
 def printCalculations(res):
-    resbybit = []
-    if res >= 0 and res <= 19:
-        print(inttostr[res])
-    else:
-        res = str(res)
-        i = len(res)
-        for elem in res:
-            resbybit.append(int(elem) * 10 ** (i-1))
-            i -= 1
-        if resbybit[-2] == 10:
-            resbybit[-2] = resbybit[-2] + resbybit[-1]
-            resbybit.pop()
-        for elem in resbybit:
-            if elem != 0:
-                print(inttostr[elem] , end = " ")
-strtoint = {"ноль": 0,
+    resbybit = [[],[]]
+    res = round(res,4)
+    res2 = str(res).split(".")
+    for i in range(2):
+        lc = len(res2[i])
+        for elem in res2[i]:
+            lc -= 1
+            resbybit[i].append(int(elem) * 10 ** (lc))
+        if len(resbybit[i]) >= 2:
+            if resbybit[i][-2] == 10:
+                resbybit[i][-2] = 10 + resbybit[i][-1]
+                resbybit[i].pop()
+    for elem in resbybit[0]:
+        print(inttostr[elem],end = " ")
+    if len([item for item in resbybit[1] if item != 0]) != 0:
+        print("и", end = " ")
+        for elem in resbybit[1]:
+            print(doubletostr[elem], end = " ")
+        if res2[1][-1] != "1":
+            print(fracword[len(str(res2[1]))-1])
+        else:
+            print(fracword2[len(str(res2[1])) - 1])
+
+fracword = ["десятых","сотых","тысячных","десятитысячных","статысячных","миллионых"]
+fracword2 = ["десятая","сотая","тысячная","десятитысячная","статысячная","миллионая"]
+strtoint = {
+            "ноль": 0,
             "один": 1,
+            "одна" : 1,
             "два": 2,
+            "две" : 2,
             "три": 3,
             "четыре": 4,
             "пять": 5,
@@ -73,11 +121,73 @@ strtoint = {"ноль": 0,
             "восемьсот": 800,
             "девятьсот": 900,
             "тысяча": 1000,
+            "две тысячи": 2000,
+            "три тысячи": 3000,
+            "четыре тысячи": 4000,
+            "пять тысяч": 5000,
+            "шесть тысяч": 6000,
+            "семь тысяч": 7000,
+            "восемь тысяч": 8000,
+            "девять тысяч": 9000,
+            "десять тысяч": 10000,
             "плюс": "+",
             "минус": "-",
             "умножить": "*",
+            "делить": "/",
+            "остаток": "%"
             }
-inttostr = {0: "ноль",
+
+doubletostr = {
+            1: "одна",
+            2: "две",
+            3: "три",
+            4: "четыре",
+            5: "пять",
+            6: "шесть",
+            7: "семь",
+            8: "восемь",
+            9: "девять",
+            10: "десять",
+            11: "одиннадцать",
+            12: "двенадцать",
+            13: "тринадцать",
+            14: "четырнадцать",
+            15: "пятнадцать",
+            16: "шестнадцать",
+            17: "семнадцать",
+            18: "восемнадцать",
+            19: "девятнадцать",
+            20: "двадцать",
+            30: "тридцать",
+            40: "сорок",
+            50: "пятьдесят",
+            60: "шестьдесят",
+            70: "семьдесят",
+            80: "восемьдесят",
+            90: "девяносто",
+            100: "сто",
+            200: "двести",
+            300: "триста",
+            400: "четыреста",
+            500: "пятьсот",
+            600: "шестьсот",
+            700: "семьсот",
+            800: "восемьсот",
+            900: "девятьсот",
+            1000: "тысяча",
+            2000: "две тысячи",
+            3000: "три тысячи",
+            4000: "четыре тысячи",
+            5000: "пять тысяч",
+            6000: "шесть тысяч",
+            7000: "семь тысяч",
+            8000: "восемь тысяч",
+            9000: "девять тысяч",
+            10000: "десять тысяч",
+            }
+
+inttostr = {
+            0: "ноль",
             1: "один",
             2: "два",
             3: "три",
@@ -115,14 +225,26 @@ inttostr = {0: "ноль",
             800: "восемьсот",
             900: "девятьсот",
             1000: "тысяча",
-            "+": "плюс",
-            "-": "минус",
-            "*": "умножить",}
+            2000: "две тысячи",
+            3000: "три тысячи",
+            4000: "четыре тысячи",
+            5000: "пять тысяч",
+            6000: "шесть тысяч",
+            7000: "семь тысяч",
+            8000: "восемь тысяч",
+            9000: "девять тысяч",
+            10000: "десять тысяч",
+            }
 
 def main():
     inp = input().split()
     res = inputCalculations(inp)
+    while res == "Неправильный Ввод" or res == "Ошибка деления на 0":
+        if res == "Неправильный Ввод":
+            print("Неправильный Ввод")
+        elif res == "Ошибка деления на 0":
+            print("Ошибка деления на 0, введите число заново")
+        inp = input().split()
+        res = inputCalculations(inp)
     printCalculations(res)
-
 main()
-# пятьдесят два плюс три
